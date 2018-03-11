e6f6f379aa825e35d4286c2874434e51deee862f
Xing Tao Shi <xshi@stuy.edu>
2018-03-11 13:30:24 -0400

    fin

--- svg.js
+++ svg.js
@@ -1,96 +1,72 @@
 var c = document.getElementById("svgg");
 var cbutton = document.getElementById("cbutton");
+var xVel = 4;
+var yVel = 6;
+var radius = 10;
+var elements = [];
+var id;
 
-var createCircle = function (x, y, r, color, svg) {
 
-	var c1 = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-	c1.setAttribute("cx", x);
-	c1.setAttribute("cy", y);
-	c1.setAttribute("r", r);
-	c1.setAttribute("fill", color);
-	c1.addEventListener('click', changeGreen, true);
-	return c1;
+var createCircle = function (e) {
+    clearInterval(id);
+    id = setInterval(animate, 10);
+    var c1 = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    c1.setAttribute("cx", e.offsetX);
+    c1.setAttribute("cy", e.offsetY);
+    c1.setAttribute("r", radius);
+    c1.setAttribute("xvel", xVel);
+    c1.setAttribute("yvel", yVel);
+    c1.setAttribute("fill", "green");
+    c.append(c1);
+    elements.push(c1);
 
 };
 
-var createRandomCircle = function(e) {
 
-	var c1 = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-	var r = 10;
-	var x = Math.floor(Math.random() * (500-r));
-	var y = Math.floor(Math.random() * (500-r));
-	c1.setAttribute("cx", x);
-	c1.setAttribute("cy", y);
-	c1.setAttribute("r", r);
-	c1.setAttribute("fill", "pink");
-	c1.addEventListener('click', changeGreen, true);
-	c.append( c1);
+var animate = function(e) {
+    var i;
+    for (i = 0; i < elements.length; i++) {
 
-	c.removeChild(this);
-	e.stopPropagation();
-};
+	var currEle = elements[i];
+	var currX = Number(currEle.getAttribute("cx"));
+	var currY = Number(currEle.getAttribute("cy"));
+	var currXVel = Number(currEle.getAttribute("xvel"));
+	var currYVel = Number(currEle.getAttribute("yvel"));
 
-var changeGreen = function(e) {
+	if (currX + radius >= 500 || currX <= 0) {
+	    currXVel *= -1;
+	}
+	else if (currY + radius >= 500 || currY <= 0) {
+	    currYVel *= -1;
+	}
 
-    //console.log("changing colora");;
-    this.setAttribute("fill", "green");
-    this.removeEventListener('click', changeGreen, true);
-    this.addEventListener('click', createRandomCircle, true);
-    e.stopPropagation();
-    
-    
-};
-
-var newCirc = function(xcor, ycor, rad, color, element){
-	var circ = {
-		x: xcor,
-		y: ycor,
-		rad: rad,
-		color: color,
-		element: document.createElementNS("http://www.w3.org/2000/svg", element),
-
-		display: function() {
-		this.element.setAttribute("cx", this.x);
-		this.element.setAttribute("cy", this.y);
-		this.element.setAttribute("r", this.rad);
-		this.element.setAttribute("fill", this.color);
-		this.element.addEventListener("click", this.change);
-		c.appendChild(this.element);
-	    },
-		change: function(e) {
-		if (this.getAttribute("fill") == "blue" || this.getAttribute("fill") == "pink") {
-		    this.setAttribute("fill", "green");
-		}
-		else {
-		    this.remove(e);
-		    var newC = newCirc(Math.random() * 500, Math.random() * 500, 10, "pink", "circle");
-		    newC.display();
-		}
-		e.stopPropagation();
-	    },
-		remove: function() {
-		svg.removeChild(this.element);
-	    }
-		
-	};
-	return circ;
-};
-
-//----------------EVENT LISTENERS---------------------
-c.addEventListener('click', function(e) {
-
-
-	var cir = newCirc(e.offsetX, e.offsetY, 10, "blue", "circle");
-	cir.display();
-	console.log(cir);
-
-    });
-
-
-cbutton.addEventListener('click', function() {
+	currX += currXVel;
+	currY += currYVel;
 	
-	while (c.firstChild) {
-	    c.removeChild(c.firstChild);
-	}	
+	currEle.setAttribute("cx", currX);
+	currEle.setAttribute("cy", currY);
+	currEle.setAttribute("xvel", currXVel);
+	currEle.setAttribute("yvel", currYVel);
 
-    });
+
+    }
+
+    console.log("hello");
+
+};
+
+
+var stop = function() {
+    while (c.firstChild) {
+	c.removeChild(c.firstChild);
+    }
+    clearInterval(id);
+    elements = [];
+};
+
+
+//----------------EVENT LISTENERS--------------------
+
+c.addEventListener('click', createCircle);
+
+cbutton.addEventListener('click', stop);
